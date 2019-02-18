0a432c71181579a8a17b851e4739bdeb8d7298d7
Harald Sagosen <HASA@statoil.com>
2019-02-18 22:49:43 +0100

    sql

---
+++ sql/Netfonds/Netfonds/hasa/Tables/obx.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [hasa].[obx] (
+    [OSEBX]     VARCHAR (20) NULL,
+    [siste]     DECIMAL (18) NULL,
+    [lav]       DECIMAL (18) NULL,
+    [høy]       DECIMAL (18) NULL,
+    [omsetning] VARCHAR (20) NULL
+);
+
