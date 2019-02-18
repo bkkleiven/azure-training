6d7ec26b3db5c1f06f7df3b9eaa947a8fd580158
Harald Sagosen <HASA@statoil.com>
2019-02-18 23:01:05 +0100

    varchar

--- sql/Netfonds/Netfonds/hasa/Tables/netfonds.sql
+++ sql/Netfonds/Netfonds/hasa/Tables/netfonds.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [hasa].[netfonds] (
-    [name]              VARCHAR (30) NULL,
+    [name]              VARCHAR (40) NULL,
     [paper]             VARCHAR (20) NULL,
     [last]              VARCHAR (20) NULL,
     [bid]               VARCHAR (20) NULL,
