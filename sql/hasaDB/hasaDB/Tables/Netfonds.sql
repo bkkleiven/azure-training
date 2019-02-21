4f31b506cf3e919002ecb83aed7c10f81f5d6818
Harald Sagosen <HASA@statoil.com>
2019-02-21 20:35:08 +0100

    ryddet

---
+++ sql/hasaDB/hasaDB/Tables/Netfonds.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [hasa].[netfonds] (
+    [name]              VARCHAR (40) NULL,
+    [paper]             VARCHAR (20) NULL,
+    [last]              VARCHAR (20) NULL,
+    [bid]               VARCHAR (20) NULL,
+    [offer]             VARCHAR (20) NULL,
+    [change]            VARCHAR (20) NULL,
+    [open]              VARCHAR (20) NULL,
+    [low]               VARCHAR (20) NULL,
+    [high]	        VARCHAR (20) NULL,
+    [prev_price]        VARCHAR (20) NULL,
+    [volume]            VARCHAR (20) NULL,
+    [bid_depth_total]   VARCHAR (20) NULL,
+    [offer_depth_total] VARCHAR (20) NULL
+);
