2fee302e465f16303ba1fb60b42ee340d89d0ac7
Harald Sagosen <HASA@statoil.com>
2019-02-22 21:49:48 +0100

    lagt til stored procedure

---
+++ sql/hasaDB/hasaDB/Programmability/Stored Procedures/Procedure1.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [hasa].[Add_staging_Netfonds]
+AS
+BEGIN
+	TRUNCATE TABLE [hasa].[Netfonds]
+	INSERT INTO [hasa].[Netfonds]
+		SELECT 
+			[name],
+			[paper],            
+			[last],       
+			[open],         
+			[low],     
+			[high],		
+			[prev_price]        
+		FROM [hasa].[netfonds_staging]
+END
--- sql/hasaDB/hasaDB/Tables/Netfonds.sql
+++ sql/hasaDB/hasaDB/Tables/Netfonds.sql
@@ -2,14 +2,8 @@
     [name]              NVARCHAR(30) NULL,
     [paper]             NVARCHAR(10) NULL,
     [last]              NVARCHAR(10) NULL,
-    [bid]               NVARCHAR(10) NULL,
-    [offer]             NVARCHAR(10) NULL,
-    [change]            NVARCHAR(10) NULL,
     [open]              NVARCHAR(10) NULL,
     [low]               NVARCHAR(10) NULL,
     [high]				NVARCHAR(10) NULL,
-    [prev_price]        NVARCHAR(10) NULL,
-    [volume]            NVARCHAR(10) NULL,
-    [bid_depth_total]   NVARCHAR(10) NULL,
-    [offer_depth_total] NVARCHAR(10) NULL
+    [prev_price]        NVARCHAR(10) NULL
 );
