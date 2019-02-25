05d3cad7886302b6ea0159e56c4bd972ef9c85d0
Harald Sagosen <HASA@statoil.com>
2019-02-25 20:41:48 +0100

    lagt til netfonds_5year

---
+++ sql/hasaDB/hasaDB/Tables/netfonds_5year.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [hasa].[netfonds_5year] (
+    [quote_date]        NVARCHAR(10) NULL,
+    [paper]             NVARCHAR(10) NULL,
+    [exch]              NVARCHAR(10) NULL,
+    [open]              NVARCHAR(10) NULL,
+	[high]				NVARCHAR(10) NULL,
+    [low]               NVARCHAR(10) NULL,
+    [close]             NVARCHAR(10) NULL,
+    [volume]		    NVARCHAR(10) NULL,
+	[value]			    NVARCHAR(10) NULL
+);
