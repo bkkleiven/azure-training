9e9392223ae156ec615f4186e2868bc19e600b27
Harald Sagosen <HASA@statoil.com>
2019-02-25 21:57:58 +0100

    nvarchar

--- sql/hasaDB/hasaDB/Tables/netfonds_5year.sql
+++ sql/hasaDB/hasaDB/Tables/netfonds_5year.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [hasa].[netfonds_5year] (
     [quote_date]        NVARCHAR(10) NULL,
     [paper]             NVARCHAR(10) NULL,
-    [exch]              NVARCHAR(10) NULL,
+    [exch]              NVARCHAR(15) NULL,
     [open]              NVARCHAR(10) NULL,
 	[high]				NVARCHAR(10) NULL,
     [low]               NVARCHAR(10) NULL,
